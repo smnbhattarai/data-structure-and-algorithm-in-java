f573370e7cacd5dd455916a89e2701101d6d25ac
suman <suman.bhattarai@gmail.com>
2021-08-03 23:40:53 +0545

    graph

--- 10-Graph/src/Graph.java
+++ 10-Graph/src/Graph.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
     private class Node {
@@ -60,5 +57,89 @@ public class Graph {
                 System.out.println(source + " is connected to " + targets);
             }
         }
+    }
+
+    public void traverseDepthFirst(String root) {
+        var node = nodes.get(root);
+        if (node == null) return;
+        traverseDepthFirst(node, new HashSet<>());
+    }
+
+    private void traverseDepthFirst(Node root, Set<Node> visited) {
+        System.out.println(root);
+        visited.add(root);
+        for (var node : adjacencyList.get(root)) {
+            if (!visited.contains(node)) {
+                traverseDepthFirst(node, visited);
+            }
+        }
+    }
+
+    public void traverseBreadthFirst(String root) {
+        var node = nodes.get(root);
+        if (node == null) return;
+        Set<Node> visited = new HashSet<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (visited.contains(current)) continue;
+            System.out.println(current);
+            visited.add(current);
+            for (var neighbour : adjacencyList.get(current)) {
+                if (!visited.contains(neighbour)) {
+                    queue.add(neighbour);
+                }
+            }
+        }
+
+    }
+
+
+    public List<String> topologicalSort() {
+        Stack<Node> stack = new Stack<>();
+        Set<Node> visited = new HashSet<>();
+        for (var node : nodes.values()) {
+            topologicalSort(node, visited, stack);
+        }
+        List<String> sorted = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            sorted.add(stack.pop().label);
+        }
+        return sorted;
+    }
+
+    private void topologicalSort(Node node, Set<Node> visited, Stack<Node> stack) {
+        if (visited.contains(node)) return;
+        visited.add(node);
+        for (var neighbour : adjacencyList.get(node)) {
+            topologicalSort(neighbour, visited, stack);
+        }
+        stack.push(node);
+    }
+
+    public boolean hasCycle() {
+        Set<Node> all = new HashSet<>();
+        all.addAll(nodes.values());
+        Set<Node> visiting = new HashSet<>();
+        Set<Node> visited = new HashSet<>();
+        while (!all.isEmpty()) {
+            var current = all.iterator().next();
+            if (hasCycle(current, all, visiting, visited)) return true;
+        }
+        return false;
+    }
+
+    private boolean hasCycle(Node node, Set<Node> all, Set<Node> visiting, Set<Node> visited) {
+        all.remove(node);
+        visiting.add(node);
+        for (var neighbour : adjacencyList.get(node)) {
+            if (visited.contains(neighbour)) continue;
+            if (visiting.contains(neighbour)) return true;
+            if (hasCycle(neighbour, all, visiting, visited)) return true;
+        }
+        visiting.remove(node);
+        visited.add(node);
+        return false;
     }
 }
--- 10-Graph/src/Main.java
+++ 10-Graph/src/Main.java
@@ -1,15 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         var graph = new Graph();
+//        graph.addNode("A");
+//        graph.addNode("B");
+//        graph.addNode("C");
+//        graph.addNode("D");
+//        graph.addEdge("A", "B");
+//        graph.addEdge("B", "D");
+//        graph.addEdge("D", "C");
+//        graph.addEdge("A", "C");
+        //graph.print();
+        //graph.removeEdge("A", "C");
+        //graph.print();
+        //graph.print();
+        //graph.traverseDepthFirst("A");
+        //graph.traverseBreadthFirst("A");
+        graph.addNode("X");
         graph.addNode("A");
         graph.addNode("B");
-        graph.addNode("C");
-        graph.addEdge("A", "B");
-        graph.addEdge("A", "C");
-        graph.print();
-        graph.removeEdge("A", "C");
-        graph.print();
-        graph.removeNode("A");
-        graph.print();
+        graph.addNode("P");
+        graph.addEdge("X", "A");
+        graph.addEdge("X", "B");
+        graph.addEdge("A", "P");
+        graph.addEdge("B", "P");
+        var list = graph.topologicalSort();
+        System.out.println(list);
     }
 }
